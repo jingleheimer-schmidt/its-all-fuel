4782a3a51be2152f13bcaf6d86ee43b90a40eeef
jingleheimer-schmidt <46385945+jingleheimer-schmidt@users.noreply.github.com>
2021-12-15 23:55:42 -0800

    first

---
+++ settings.lua
@@ -0,0 +1,51 @@
+
+data:extend({
+  {
+    type = "bool-setting",
+    name = "its-all-fuel_ores",
+    setting_type = "startup",
+    default_value = "true",
+  },
+  {
+    type = "bool-setting",
+    name = "its-all-fuel_plates",
+    setting_type = "startup",
+    default_value = "true",
+  },
+  {
+    type = "bool-setting",
+    name = "its-all-fuel_ammo",
+    setting_type = "startup",
+    default_value = "true",
+  },
+  {
+    type = "bool-setting",
+    name = "its-all-fuel_guns",
+    setting_type = "startup",
+    default_value = "true",
+  },
+  {
+    type = "bool-setting",
+    name = "its-all-fuel_equipment",
+    setting_type = "startup",
+    default_value = "true",
+  },
+  {
+    type = "bool-setting",
+    name = "its-all-fuel_armor",
+    setting_type = "startup",
+    default_value = "true",
+  },
+  {
+    type = "bool-setting",
+    name = "its-all-fuel_blueprints",
+    setting_type = "startup",
+    default_value = "true",
+  },
+  {
+    type = "bool-setting",
+    name = "its-all-fuel_everything-else",
+    setting_type = "startup",
+    default_value = "true",
+  }
+})
